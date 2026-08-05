848214179dd4708242b41a68d2c2c475649bb82c
Özgür Yaman <yamanozgur@gmail.com>
2026-08-05 15:11:01 +0300

    refactor: update base path and asset links
    Change base path to relative and update local file references to support deployment to any directory.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/vifloweb/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
